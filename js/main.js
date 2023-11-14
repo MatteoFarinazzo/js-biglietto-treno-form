ca0ae791d3821bc0b52703640b78400dc7e54f93
MatteoFarinazzo <matteofary@gmail.com>
2023-11-14 15:35:29 +0100

    added ticket features

--- js/main.js
+++ js/main.js
@@ -107,13 +107,25 @@ function userHasFinishDataCompletion(event) {
     else if (age>=65) {discountedPrice = (rangeKilometers *0.21) + "Scontato" + '&nbsp' + ((rangeKilometers *0.21) / 100 * 60).toFixed(2);}
     else {discountedPrice = (rangeKilometers *0.21).toFixed; }
 
+    let ageTipe;
+    if (age<18){ageTipe = "Minorenne";}
+    else if (age>=65){ageTipe = "Over 65";}
+    else {ageTipe = "maggiorenne";}
+
+    let min = 1;
+    let max = 12;
+
+    let random = Math.floor(Math.random() * (max - min) + min);
 
     
     document.getElementById("name").innerHTML = name;
     document.getElementById("surname").innerHTML = surname;
+    document.getElementById("age").innerHTML = age;
+    document.getElementById("ageTipe").innerHTML = ageTipe;
     document.getElementById("distance").innerHTML = rangeKilometers;
+    document.getElementById("wagon").innerHTML = random;
     document.getElementById("price").innerHTML = discountedPrice;
 
     // Tolgo il form, una volta che l'utente ha finito di completarlo
-    document.getElementById("form_data_input").classList.add("d-none");
+   
 }
